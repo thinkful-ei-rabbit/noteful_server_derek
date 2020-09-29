97c42ea1e9ae6616ce6166aa7c6693d697981ee7
Malik DeJean <mdejean@MacBook-Pro.local>
2020-09-29 09:47:10 -0700

    refactored server to work with noteful client

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   CORS_ORIGIN: process.env.CORS_ORIGIN,
-  DB_URL: process.env.DB_URL,
-  DB_TEST_URL: process.env.DB_TEST_URL
+  DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
 };
